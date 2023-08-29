4baa5770584382437fb8f2ffdf432c86bd4789b0
Svidinskiy <100801664+Svidinskiy@users.noreply.github.com>
2023-08-29 14:41:49 +0400

    Исправлено ДЗ 4.2

--- src/main/java/ru/hogwarts/school/sql/scripts421/scripts423.sql
+++ src/main/java/ru/hogwarts/school/sql/scripts421/scripts423.sql
@@ -3,6 +3,6 @@ FROM student s
 JOIN faculty f ON s.faculty_id = f.id
 WHERE f.name = 'Хогвартс';
 
-SELECT s.name AS student_name, a.filePath AS avatar_filePath
+SELECT s.name AS student_name, a.file_Path AS avatar_filePath
 FROM student s
 JOIN avatar a ON s.id = a.student_id;
